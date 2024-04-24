76c3c268beb4b2fb4e3eecbbdadb095ad44f7c95
jigneshsharma123 <92665503+jigneshsharma123@users.noreply.github.com>
2024-04-24 07:43:56 +0530

    Add files via upload
    added frontend part of the bookStore Projects

---
+++ Server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
+const app = express();
+
+app.use(cors({
+    origin : ["https://deploy-mern-1whq.vercel.app"],
+    methods : ["POST", "GET"],
+    credentials : true,
+}));
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = "mongodb+srv://jigneshsharma9868:2K2sJC3pSWgHNor8@bookstore.vefqnsi.mongodb.net/?retryWrites=true&w=majority&appName=BookStore";
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
